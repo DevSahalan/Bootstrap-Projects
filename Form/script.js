633250dc0695228061fe2b4c6d288f563c760ad2
sahalan hasan <mythsahalan@yahoo.com>
2020-12-07 10:10:05 +0600

    7dec2020

---
+++ Form/script.js
@@ -0,0 +1,8 @@
+// const name = document.getElementById('name')
+// const submit = document.getElementById('submit-btn')
+// submit.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const nameVal = name.value
+//   console.log(nameVal)
+//   alert(nameVal)
+// })
