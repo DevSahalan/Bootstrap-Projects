dd3d977355ae4e5d93cb8eb1750b5bbc90e62c0f
sahalan hasan <mythsahalan@yahoo.com>
2020-12-19 04:34:58 +0600

    18 dec 2020

--- Admin-skillshare/script.js
+++ Admin-skillshare/script.js
@@ -1,6 +1,10 @@
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-toggle="tooltip"]')
-)
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+// var tooltipTriggerList = [].slice.call(
+//   document.querySelectorAll('[data-toggle="tooltip"]')
+// )
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
+
+const numbers = [1, 2, 3, 4]
+const newNumb = numbers.map((x) => x + 1)
+console.log(newNumb)
